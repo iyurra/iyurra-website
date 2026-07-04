967c77cb5b90d81261424bf5ef6189e29c22d05d
iyurra <iyurranutrimix@gmail.com>
2026-07-04 13:25:23 +0530

    Initial Iyurra Website

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Iyurra Website Loaded Successfully 🌿");
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
