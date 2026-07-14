266dae0d1c09007beb0a95c0afb1957c5c422988
iyurra <iyurranutrimix@gmail.com>
2026-07-15 00:00:29 +0530

    Updated hero slider and mobile navigation

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ const navMenu = document.getElementById("navMenu");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+const heroSwiper = new Swiper(".heroSwiper", {
+    loop: true,
+    speed: 1200,
+
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    effect: "fade",
+
+    fadeEffect: {
+        crossFade: true,
+    },
+});
